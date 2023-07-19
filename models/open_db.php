bf4686e93dfc04c0f8a4bdad4d52cc1fcc49de12
Taavi Toomsalu <queenshelob@mail.com>
2023-07-19 12:31:42 +0300

    Uploading project

---
+++ models/open_db.php
@@ -0,0 +1,11 @@
+<?php
+
+$server = 'localhost';
+$user = 'root';
+$pass = 'root';
+$db = 'tagcloud';
+
+//make the connection by creating mysqli object
+$mysqli = new mysqli($server, $user, $pass, $db);
+
+?>
---
+++ models/tagcloud.model.php
@@ -0,0 +1,121 @@
+<?php
+
+class tagcloud_model
+{
+    var $mysqli;
+    var $tags; //array(id, name, total);
+    var $largest;
+
+    function __construct($conn)
+    {
+        $this->mysqli = $conn;
+
+        //get tags and largest value
+        $this->get_tags();
+    }
+
+    /* Utility functions 
+    -----------------------------------------------------*/
+
+    function get_tags()
+    {
+        $this->tags = array();
+        $this->largest = 0;
+
+        $result = $this->mysqli->query("SELECT posts_to_tags.tag_id, tags.name, COUNT(posts_to_tags.tag_id) AS total FROM tags, posts_to_tags WHERE posts_to_tags.tag_id = tags.id GROUP BY posts_to_tags.tag_id");
+
+        if ($result->num_rows > 0)
+        {
+            while ($row = $result->fetch_object())
+            {
+                //check for largest
+                if ($row->total > $this->largest) { $this->largest = $row->total; }
+
+                //add tag to array
+                $this->tags[] = array ('id' => $row->tag_id, 'name' => $row->name, 'total' => $row->total);
+
+            }
+
+            //sort tags
+            usort($this->tags, array('tagcloud_model', 'compare_names'));
+        }
+        else
+        {
+            $this->tags = FAlSE;
+        }
+    }
+
+    function compare_names($a, $b)
+    {
+        return strcmp($a['name'], $b['name']);
+    }
+
+    /* Display functions 
+    -----------------------------------------------------*/
+
+    function get_tag_list()
+    {
+        if ($this->tags != FALSE)
+        {
+            $data = '';
+
+            //display tags in table
+            $data .= "<table border='1' cellpadding='10'>";
+            $data .= "<tr><th>ID</th><th>Tag Name</th><th>Count</th><th>Weight</th></tr>";
+
+            foreach ($this->tags as $tag)
+            {
+                //find weight
+                $weight = round(($tag['total'] / $this->largest) * 10);
+
+                $data .= "<tr>";
+                $data .= "<td>" . $tag['id'] . "</td>";
+                $data .= "<td>" . $tag['name'] . "</td>";
+                $data .= "<td>" . $tag['total'] . "</td>";
+                $data .= "<td>" . $weight . "</td>";
+                $data .= "</tr>";
+
+            }
+
+            $data .= "</table>";
+            return $data;
+        }
+        else
+        {
+            return "No tags to display!";
+        }
+    }
+
+    function get_tag_cloud()
+    {
+
+        if ($this->tags != FALSE)
+        {
+            //create unordered list
+            $data = '';
+
+            $data .= '<ul class="tagcloud">';
+
+            foreach($this->tags as $tag)
+            {
+                //find weight
+                $weight = round(($tag['total'] / $this->largest) * 10);
+
+                //create list item
+                $data .= "<li><a href='tags.php?id=". $tag['id'] ."' class='tag". $weight  ."'>";
+                $data .= $tag['name'] . "<a></li>";
+                $data .= "\n";
+            }
+
+            $data .= "</ul>";
+            return $data;
+        }
+        else
+        {
+            return "No tags to display!";
+        }
+    }
+    
+}
+
+?>
